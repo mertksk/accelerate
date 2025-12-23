a2a8f5b86dac4602de2c7c3d365272a98da1bd7f
mertksk <mert_ksk_1998@hotmail.com>
2025-12-23 16:19:37 +0100

    build: upgrade Next.js to 16 and React to 19

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  },
+  typedRoutes: true,
   output: "standalone",
+  serverExternalPackages: ['snarkjs', 'circomlibjs'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'child_process': 'commonjs child_process',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
